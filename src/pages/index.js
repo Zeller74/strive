2894df84936982a4d889c2d4c2f21da8efd721d0
Jeff <plmjo63@gmail.com>
2023-10-18 20:32:59 -0400

    Select group chat + join group

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,91 @@ const ChatMessages = ({ messages, setMessages }) => {
   );
 };
 
-function SendMessageForm({ messages, setMessages, refreshMessages }) {
+function StudyGroups({ selectedStudyGroup, setSelectedStudyGroup }) {
+  const { getToken, userId } = useAuth();
+  const [groups, setGroups] = useState([]);
+  const [userGroups, setUserGroups] = useState([]);
+
+  useEffect(() => {
+    const fetchGroups = async () => {
+      const supabaseAccessToken = await getToken({ template: "supabase" });
+      const supabase = await supabaseClient(supabaseAccessToken);
+  
+      // Fetch all study groups
+      const { data: allGroupsData } = await supabase
+        .from("study_group")
+        .select("*");
+  
+      // Fetch only the study groups where the user is enrolled
+      const { data: userEnrollments } = await supabase
+        .from("enrollment")
+        .select("study_group")
+        .eq("user_id", userId);
+  
+      setGroups(allGroupsData);  // This will set the list of ALL study groups
+      setUserGroups(userEnrollments.map(e => e.study_group));  // This will set only the ones user is part of
+    };
+    
+    fetchGroups();
+  }, [userId]);
+  
+
+
+  const joinGroup = async (groupId) => {
+    const supabaseAccessToken = await getToken({ template: "supabase" });
+    const supabase = await supabaseClient(supabaseAccessToken);
+    await supabase
+      .from("enrollment")
+      .insert({ user_id: userId, study_group: groupId });
+    setUserGroups([...userGroups, groupId]);
+  };
+
+  const leaveGroup = async (groupId) => {
+    const supabaseAccessToken = await getToken({ template: "supabase" });
+    const supabase = await supabaseClient(supabaseAccessToken);
+    await supabase
+      .from("enrollment")
+      .delete()
+      .eq("user_id", userId)
+      .eq("study_group", groupId);
+    setUserGroups(userGroups.filter((g) => g !== groupId));
+  };
+
+  return (
+    <div>
+      <h3>Study Groups</h3>
+      <select
+        value={selectedStudyGroup}
+        onChange={(e) => setSelectedStudyGroup(Number(e.target.value))}
+      >
+        {userGroups.map((groupId) => {
+  const group = groups.find((g) => g.id === groupId);
+  if (!group) return null;
+  return (
+    <option key={group.id} value={group.id}>
+      {group.name}
+    </option>
+  );
+})}
+
+      </select>
+      <ul>
+        {groups.map((group) => (
+          <li key={group.id}>
+            {group.name}
+            {userGroups.includes(group.id) ? (
+              <button onClick={() => leaveGroup(group.id)}>Leave</button>
+            ) : (
+              <button onClick={() => joinGroup(group.id)}>Join</button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function SendMessageForm({ messages, setMessages, refreshMessages, selectedStudyGroup }) {
   const { getToken, userId } = useAuth();
   const [newMessage, setNewMessage] = useState("");
   const { session } = useSession();
@@ -50,9 +134,8 @@ function SendMessageForm({ messages, setMessages, refreshMessages }) {
 
       const { data: userData } = await supabase
         .from("user")
-        .select("study_group")
+        .select("name")
         .eq("user_id", userId);
-      setStudyGroup(userData[0]?.study_group);
     };
 
     fetchStudyGroup();
@@ -72,7 +155,7 @@ function SendMessageForm({ messages, setMessages, refreshMessages }) {
       .insert({
         text: newMessage,
         user_id: userId,
-        study_group: studyGroup ?? 0,
+        study_group: selectedStudyGroup ?? 0,
       })
       .single();
 
@@ -149,7 +232,7 @@ export default function Home() {
 
       const { data: usersData, error } = await supabase
         .from("user")
-        .select("study_group")
+        .select("name")
         .eq("user_id", userId); // Using "user.id" directly
 
       if (error) {
@@ -161,7 +244,7 @@ export default function Home() {
         // User does not exist, create a new record
         await supabase
           .from("user")
-          .insert({ user_id: userId, study_group: 0, name: user.firstName });
+          .insert({ user_id: userId, name: user.firstName });
       }
     }
 
@@ -169,7 +252,6 @@ export default function Home() {
   }, [isLoading, isSignedIn, user, getToken]); // Added dependencies here
 
   useEffect(() => {
-
     fetchMessagesForGroup();
     // Set up an interval to call it every 5 seconds
     const interval = setInterval(fetchMessagesForGroup, 5000);
@@ -189,10 +271,15 @@ export default function Home() {
             {isSignedIn ? (
               <>
                 <div className={styles.label}>Welcome {user.firstName}!</div>
+                <StudyGroups
+                  selectedStudyGroup={selectedStudyGroup}
+                  setSelectedStudyGroup={setSelectedStudyGroup}
+                />
                 <SendMessageForm
                   messages={messages}
                   setMessages={setMessages}
                   refreshMessages={fetchMessagesForGroup}
+                  selectedStudyGroup={selectedStudyGroup}
                 />
                 <ChatMessages messages={messages} setMessages={setMessages} />
               </>
