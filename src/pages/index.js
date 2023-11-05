59597b0ec2535bbdfd4426ab0df29961ab11b79f
Jeff <plmjo63@gmail.com>
2023-11-05 16:37:41 -0500

    search

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import styles from "../styles/Home.module.css";
 import { useState, useEffect, useRef } from "react";
-import Modal from 'react-modal';
+import Modal from "react-modal";
 
 import {
   useAuth,
@@ -24,12 +24,16 @@ import {
 
 //import icons from react icons
 import { FaList, FaRegHeart } from "react-icons/fa";
-import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import {
+  FiHome,
+  FiLogOut,
+  FiArrowLeftCircle,
+  FiArrowRightCircle,
+} from "react-icons/fi";
 import { RiPencilLine } from "react-icons/ri";
 import { BiCog } from "react-icons/bi";
 
-
-//import sidebar css from react-pro-sidebar module and our custom css 
+//import sidebar css from react-pro-sidebar module and our custom css
 import "react-pro-sidebar/dist/css/styles.css";
 
 const supabaseClient = async (supabaseAccessToken) => {
@@ -44,36 +48,55 @@ const supabaseClient = async (supabaseAccessToken) => {
   return supabase;
 };
 
-const SearchGroups = ({ getToken, isVisible, onClose }) => {
+const SearchGroups = ({ isVisible, onClose}) => {
+  const { getToken, userId } = useAuth();
   const [searchTerm, setSearchTerm] = useState('');
   const [groups, setGroups] = useState([]);
+  const [userGroups, setUserGroups] = useState([]); // To store the user's groups
 
   useEffect(() => {
-    const fetchGroups = async () => {
+    const fetchGroupsAndUserGroups = async () => {
+      if (!isVisible) return;
+
       const supabaseAccessToken = await getToken({ template: "supabase" });
       const supabase = await supabaseClient(supabaseAccessToken);
 
+      // Fetch all groups
       const { data: allGroupsData } = await supabase
         .from('study_group')
         .select('*');
-
       setGroups(allGroupsData);
+
+      // Fetch the groups the user is a member of
+      const { data: userGroupsData } = await supabase
+        .from('enrollment')
+        .select('study_group')
+        .eq('user_id', userId);
+      setUserGroups(userGroupsData.map((enrollment) => enrollment.study_group));
     };
 
-    if (isVisible) {
-      fetchGroups();
-    }
-  }, [isVisible]);
+    fetchGroupsAndUserGroups();
+  }, [isVisible, getToken, userId]);
+
+  const joinGroup = async (groupId) => {
+    const supabaseAccessToken = await getToken({ template: "supabase" });
+    const supabase = await supabaseClient(supabaseAccessToken);
+    await supabase
+      .from("enrollment")
+      .insert({ user_id: userId, study_group: groupId });
+
+    setUserGroups([...userGroups, groupId]);
+  };
+
+  const isUserMember = (groupId) => {
+    return userGroups.includes(groupId);
+  };
 
   const filteredGroups = searchTerm
     ? groups.filter((group) =>
         group.name.toLowerCase().includes(searchTerm.toLowerCase())
       )
     : groups;
-
-  const joinGroup = async (groupId) => {
-    // Functionality to join a group
-  };
 
   if (!isVisible) return null;
 
@@ -90,13 +113,16 @@ const SearchGroups = ({ getToken, isVisible, onClose }) => {
         {filteredGroups.map((group) => (
           <div key={group.id} className="group-item">
             {group.name}
-            <button onClick={() => joinGroup(group.id)}>Join</button>
+            {!isUserMember(group.id) && (
+              <button onClick={() => joinGroup(group.id)}>Join</button>
+            )}
           </div>
         ))}
       </div>
     </div>
   );
 };
+
 
 const ChatMessages = ({ messages, setMessages }) => {
   const containerRef = useRef(null);
@@ -199,32 +225,28 @@ function StudyGroups({ selectedStudyGroup, setSelectedStudyGroup }) {
   };
 
   //create initial menuCollapse state using useState hook
-  const [menuCollapse, setMenuCollapse] = useState(false)
+  const [menuCollapse, setMenuCollapse] = useState(false);
 
   //create a custom function that will change menucollapse state from false to true and true to false
   const menuIconClick = () => {
-  //condition checking to change state from true to false and vice versa
-  menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
+    //condition checking to change state from true to false and vice versa
+    menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
   };
-  
+
   return (
     <div id="header">
-    <ProSidebar collapsed={menuCollapse}>
-      <SidebarHeader>
-      <div className="logotext">
-          <p>{menuCollapse ? "Groups" : "Groups"}</p>
-        </div>
-        <div className="closemenu" onClick={menuIconClick}>
-          {menuCollapse ? (
-            <FiArrowRightCircle/>
-          ) : (
-            <FiArrowLeftCircle/>
-          )}
-        </div>
-      </SidebarHeader>
-      <SidebarContent>
-        <Menu iconShape="square">
-          {userGroups.map((groupId) => {
+      <ProSidebar collapsed={menuCollapse}>
+        <SidebarHeader>
+          <div className="logotext">
+            <p>{menuCollapse ? "Groups" : "Groups"}</p>
+          </div>
+          <div className="closemenu" onClick={menuIconClick}>
+            {menuCollapse ? <FiArrowRightCircle /> : <FiArrowLeftCircle />}
+          </div>
+        </SidebarHeader>
+        <SidebarContent>
+          <Menu iconShape="square">
+            {userGroups.map((groupId) => {
               const group = groups.find((g) => g.id === groupId);
               if (!group) return null;
               return (
@@ -244,28 +266,24 @@ function StudyGroups({ selectedStudyGroup, setSelectedStudyGroup }) {
               );
             })}
             <ul>
-            {groups.map((group) => (
-              <li key={group.id}>
-                {userGroups.includes(group.id) ? (
-                  <MenuItem
-                    onClick={() => leaveGroup(group.id)}
-                  >
-                    Leave {group.name}
-                  </MenuItem>
-                ) : (
-                  <MenuItem
-                    onClick={() => joinGroup(group.id)}
-                  >
-                    Join {group.name}
-                  </MenuItem>
-                )}
-              </li>
-            ))}
-          </ul>
-        </Menu>
-      </SidebarContent>
-    </ProSidebar>
-</div>
+              {groups.map((group) => (
+                <li key={group.id}>
+                  {userGroups.includes(group.id) ? (
+                    <MenuItem onClick={() => leaveGroup(group.id)}>
+                      Leave {group.name}
+                    </MenuItem>
+                  ) : (
+                    <MenuItem onClick={() => joinGroup(group.id)}>
+                      Join {group.name}
+                    </MenuItem>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </Menu>
+        </SidebarContent>
+      </ProSidebar>
+    </div>
   );
 }
 
@@ -318,17 +336,16 @@ function SendMessageForm({
     refreshMessages();
   };
 
-return (
-  <form className={styles.sendMessageForm} onSubmit={handleSubmit}>
-    <input
-      className={styles.chatInput}
-      onChange={(e) => setNewMessage(e.target.value)}
-      value={newMessage}
-    />
-    <button className={styles.sendButton}>Send</button>
-  </form>
-);
-
+  return (
+    <form className={styles.sendMessageForm} onSubmit={handleSubmit}>
+      <input
+        className={styles.chatInput}
+        onChange={(e) => setNewMessage(e.target.value)}
+        value={newMessage}
+      />
+      <button className={styles.sendButton}>Send</button>
+    </form>
+  );
 }
 
 export default function Home() {
@@ -342,7 +359,6 @@ export default function Home() {
   const toggleSearch = () => {
     setSearchVisible(!isSearchVisible);
   };
-
 
   async function fetchMessagesForGroup() {
     const supabaseAccessToken = await getToken({ template: "supabase" });
@@ -455,37 +471,47 @@ export default function Home() {
                   </div>
                   <button onClick={toggleSearch}>Find Group</button>
 
-      {/* Search Section */}
-      <div style={{ display: isSearchVisible ? 'block' : 'none' }}>
-        <SearchGroups
-          getToken={getToken}
-          isVisible={isSearchVisible}
-          onClose={toggleSearch}
-        />
-      </div>
+                  {/* Search Section */}
+                  <div style={{ display: isSearchVisible ? "block" : "none" }}>
+                    <SearchGroups
+                      isVisible={isSearchVisible}
+                      onClose={toggleSearch}
+                    />
+                  </div>
                 </div>
               </>
             ) : (
               <>
                 <div className={styles.welcome}>
-                  <p>Succeed Together <br></br>Join virtual study groups tailord to your subjects and thrive together</p>
+                  <p>
+                    Succeed Together <br></br>Join virtual study groups tailord
+                    to your subjects and thrive together
+                  </p>
                   <div className={styles.welcomeSignUp}>
                     <SignUpButton />
                   </div>
                 </div>
-              <div className ={styles.descriptions}>
-                    <p>Dive into seamless collaboration <br></br>with integrated video sessions, <br></br>whiteboards
-                      , and chat. Turn<br></br>study hours into interactive <br></br>brainstorming sessions
-                    </p>
-                    <p>Discover groups that match <br></br>your courses and interests.<br></br>
-                      Whether it's calculus or<br></br>classis literature, there's a<br></br>
-                      squad waiting for you.
-                    </p>
-                    <p>Success a trasure trove of <br></br>shard notes, practice papers<br></br>
-                      and study materials. Every<br></br>group member contributes,<br></br>and everyone benefits!
-                    </p>
-                  
-                </div>  
+                <div className={styles.descriptions}>
+                  <p>
+                    Dive into seamless collaboration <br></br>with integrated
+                    video sessions, <br></br>whiteboards , and chat. Turn
+                    <br></br>study hours into interactive <br></br>brainstorming
+                    sessions
+                  </p>
+                  <p>
+                    Discover groups that match <br></br>your courses and
+                    interests.<br></br>
+                    Whether it's calculus or<br></br>classis literature, there's
+                    a<br></br>
+                    squad waiting for you.
+                  </p>
+                  <p>
+                    Success a trasure trove of <br></br>shard notes, practice
+                    papers<br></br>
+                    and study materials. Every<br></br>group member contributes,
+                    <br></br>and everyone benefits!
+                  </p>
+                </div>
               </>
             )}
           </div>
